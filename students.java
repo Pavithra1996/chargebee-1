713024dc01167b4b841055ede5fce758ba4bf49c
Tharanipriya21 <tharanipriya900@gmail.com>
2016-07-10 11:50:54 +0530

    Create students.java

---
+++ students.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class students
+{
+public static void main(String args[])
+{
+int n,x,p,i,j=1;
+System.out.println("Enter n value");
+scanner sc=new sc(System.in);
+n=sc.nextInt();
+System.out.println("give pair");
+p=sc.nextInt();
+for(i=0;i<2*n;i++)
+{
+x=2*n-i;
+if(p!=x)
+System.out.println("The pair is for days is "+p "+" j++ "+" p);
+}
+}
+}
